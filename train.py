b924e7f5d14da35b3ba4c9f8d691c9d872c70256
zpltys <gzpltys@gmail.com>
2019-03-23 17:28:24 +0800

    test

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ MOVING_AVERAGE_DECAY = 0.99
 MODEL_SAVE_PATH = "model/"
 MODEL_NAME = "test"
 
-def train(train_images, train_labels):
+def train(train_images, train_labels, test_images, test_labels):
     with tf.device('/CPU:0'):
         x = tf.placeholder(tf.float32, [BATCH_SIZE, inference.IMAGE_SIZE,
                                         inference.IMAGE_SIZE,
@@ -32,14 +32,11 @@ def train(train_images, train_labels):
 
         global_step = tf.Variable(0, trainable=False)
 
-        variable_average = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+        #variable_average = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
 
-        print('trainable variables:')
-        for v in tf.trainable_variables():
-            print(v)
 
-        variable_average_op = variable_average.apply(tf.trainable_variables())
-        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.argmax(y_, 1), logits=y)
+        #variable_average_op = variable_average.apply(tf.trainable_variables())
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
         cross_entropy_mean = tf.reduce_mean(cross_entropy)
 
         loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
@@ -50,7 +47,8 @@ def train(train_images, train_labels):
                                                    decay_rate=LEARNING_RATE_DECAY)
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
-        with tf.control_dependencies([train_step, variable_average_op]):
+        #with tf.control_dependencies([train_step, variable_average_op]):
+        with tf.control_dependencies([train_step]):
             train_op = tf.no_op(name='train')
 
         saver = tf.train.Saver()
@@ -67,12 +65,25 @@ def train(train_images, train_labels):
                 xs = np.reshape(xs, [BATCH_SIZE, inference.IMAGE_SIZE,
                                      inference.IMAGE_SIZE,
                                      inference.NUM_CHANNELS])
-                _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_o: ys})
+                _, loss_value, step, y_predict = sess.run([train_op, loss, global_step, y], feed_dict={x: xs, y_o: ys})
                 if i % 10 == 0:
                     print("After %d training steps, loss on training batch is %g" % (step, loss_value))
+                    #print(type(y_predict))
+                    y_predict = y_predict.argmax(1)
+                    count = 0
+                    for index in range(BATCH_SIZE):
+                        if y_predict[index] == ys[index]:
+                            count += 1
+                    print("rate is %f%%" % (count / BATCH_SIZE))
+
+                if i % 1000 == 0:
                     saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 if __name__ == '__main__':
     train_images = read.load_train_images()
     train_labels = read.load_train_labels()
-    train(train_images, train_labels)
+
+    test_images = read.load_test_images()
+    test_labels = read.load_test_labels()
+
+    train(train_images, train_labels, test_images, test_labels)
